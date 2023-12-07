10fbdd86d220ad65081d31c18d45625e2a50b42e
wildhanstrdy <wildhansatriady@gmail.com>
2023-12-07 16:23:00 +0700

    add get popular movies service

--- composeApp/src/commonMain/kotlin/api/MovieService.kt
+++ composeApp/src/commonMain/kotlin/api/MovieService.kt
@@ -1,16 +1,24 @@
 package api
 
+import com.rickclephas.kmp.nativecoroutines.NativeCoroutines
+import dto.MovieListResponse
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
 interface MovieService {
-    suspend fun getMovieList()
+    @NativeCoroutines
+    suspend fun getPopularMovies(page: Int): MovieListResponse
 }
 
-internal class MovieServiceImpl constructor(
+private const val TMDB_BASE_URL = "https://api.themoviedb.org/3"
+
+internal class MovieServiceImpl(
     private val httpClient: HttpClient
 ) : MovieService {
-    override suspend fun getMovieList() {
-        TODO("Not yet implemented")
+
+    override suspend fun getPopularMovies(page: Int): MovieListResponse {
+        return httpClient.get("$TMDB_BASE_URL/movie/popular").body()
     }
 
 }
