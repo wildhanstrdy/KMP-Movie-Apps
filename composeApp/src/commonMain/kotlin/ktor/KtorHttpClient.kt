9820fa0a844d09f823c3f97746a7c3793ffae97f
wildhanstrdy <wildhansatriady@gmail.com>
2023-12-07 16:22:06 +0700

    install ktor logger, auth, and json parser plugin

--- composeApp/src/commonMain/kotlin/ktor/KtorHttpClient.kt
+++ composeApp/src/commonMain/kotlin/ktor/KtorHttpClient.kt
@@ -3,6 +3,18 @@ package ktor
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.engine.cio.endpoint
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.HttpHeaders
+import io.ktor.serialization.kotlinx.json.json
+import keys.API_KEYS
+import kotlinx.serialization.json.Json
 
 abstract class KtorHttpClient {
     abstract fun getHttpClient(): HttpClient
@@ -11,6 +23,24 @@ abstract class KtorHttpClient {
 class CIOHttpClient : KtorHttpClient() {
     override fun getHttpClient(): HttpClient {
         return HttpClient(CIO) {
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.HEADERS
+                sanitizeHeader { header -> header == HttpHeaders.Authorization }
+            }
+            install(Auth) {
+                bearer {
+                    loadTokens {
+                        BearerTokens(API_KEYS.TMDB_KEY, API_KEYS.TMDB_KEY)
+                    }
+                }
+            }
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
             engine {
                 endpoint {
                     maxConnectionsCount = 1000
@@ -26,7 +56,7 @@ class CIOHttpClient : KtorHttpClient() {
         }
     }
 
-    companion object{
+    companion object {
         const val TAG = "cioHttpClient"
     }
 }
